8a64c60bcee457dac2ebf5654c9bc3c903ef5b00
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2021-10-19 15:56:56 +0300

    19.10.2021

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = axios.create({
+  baseURL: `http://localhost:3000/courses/`,
+});
+
+class App extends Component {
+  state = {
+    courses: [],
+  };
+
+  constructor() {
+    super();
+    this.getCourses();
+  }
+
+  getCourses = async () => {
+    try {
+      let data = await api.get("/").then(({ data }) => data);
+      this.setState({ courses: data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  createCourse = async () => {
+    let res = await api
+      .post("/", {
+        title: "Test",
+        id: 4,
+        author: "Ertuğrul Emre Cihan",
+      })
+      .catch((err) => console.log(err));
+    console.log(res);
+    this.getCourses();
+  };
+
+  deleteCourse = async (id) => {
+    let data = await api.delete(`/${id}`);
+    this.getCourses();
+  };
+
+  updateCourse = async (id, val) => {
+    let data = await api.patch(`/${id}`, { title: val });
+    this.getCourses();
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick={this.createCourse}>Create New Course</button>
+          {this.state.courses.map((course) => (
+            <h2
+              key={course.id}
+              onClick={() => this.updateCourse(course.id, `${course.title}a`)}
+            >
+              {course.title}
+              <button onClick={() => this.deleteCourse(course.id)}>X</button>
+            </h2>
+          ))}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
